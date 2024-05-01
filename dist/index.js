9277e92b7e4c19cb37c96621399269d991a37271
Gan <github@yhg.io>
2024-05-01 15:47:01 +0100

    init commit

---
+++ dist/index.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._if = void 0;
+var CommandType;
+(function (CommandType) {
+    CommandType[CommandType["IF"] = 0] = "IF";
+    CommandType[CommandType["THEN"] = 1] = "THEN";
+    CommandType[CommandType["ELSE"] = 2] = "ELSE";
+    CommandType[CommandType["ELSEIF"] = 3] = "ELSEIF";
+})(CommandType || (CommandType = {}));
+;
+function _if(condition) {
+    const commands = [];
+    function _then(component) {
+        commands.push({
+            command: CommandType.THEN,
+            argument: component
+        });
+        return {
+            else: _else,
+            elseif: _elseif,
+            endif: _endif,
+        };
+    }
+    function _else(component) {
+        commands.push({
+            command: CommandType.ELSE,
+            argument: component
+        });
+        return {
+            endif: _endif,
+        };
+    }
+    function _elseif(condition) {
+        commands.push({
+            command: CommandType.ELSEIF,
+            argument: condition
+        });
+        return {
+            then: _then,
+        };
+    }
+    function _endif() {
+        let last_condition;
+        for (var i = 0; i < commands.length; i++) {
+            switch (commands[i].command) {
+                case CommandType.IF:
+                case CommandType.ELSEIF:
+                    last_condition = commands[i].argument;
+                    break;
+                case CommandType.THEN:
+                    if (last_condition === true) {
+                        return commands[i].argument;
+                    }
+                    break;
+                case CommandType.ELSE:
+                    if (last_condition === false) {
+                        return commands[i].argument;
+                    }
+                    break;
+                default:
+                    throw 'invalid syntax';
+            }
+        }
+    }
+    commands.push({
+        command: CommandType.IF,
+        argument: condition
+    });
+    return {
+        then: _then,
+    };
+}
+exports._if = _if;
---
+++ index.ts
@@ -0,0 +1,86 @@
+import React from "react";
+
+enum CommandType {
+  IF,
+  THEN,
+  ELSE,
+  ELSEIF,
+};
+
+type CommandEntry = {
+  command: CommandType,
+  argument: boolean | React.ReactNode | null
+};
+
+export function _if(condition: boolean) {
+  const commands: CommandEntry[] = [];
+
+  function _then(component: React.ReactNode) {
+    commands.push({
+      command: CommandType.THEN,
+      argument: component
+    });
+
+    return {
+      else: _else,
+      elseif: _elseif,
+      endif: _endif,
+    };
+  }
+  
+  function _else(component: React.ReactNode) {
+    commands.push({
+      command: CommandType.ELSE,
+      argument: component
+    });
+
+    return {
+      endif: _endif,
+    }
+  }
+
+  function _elseif(condition: boolean) {
+    commands.push({
+      command: CommandType.ELSEIF,
+      argument: condition
+    });
+
+    return {
+      then: _then,
+    }
+  }
+  
+  function _endif() {
+    let last_condition;
+    for (var i = 0; i < commands.length; i++) {
+      switch (commands[i].command) {
+        case CommandType.IF:
+        case CommandType.ELSEIF:
+          last_condition = commands[i].argument;
+          break;
+        case CommandType.THEN:
+          if (last_condition === true) {
+            return commands[i].argument;
+          }
+          break;
+        case CommandType.ELSE:
+          if (last_condition === false) {
+            return commands[i].argument;
+          }
+          break;
+        default:
+          throw 'invalid syntax';
+      }
+    }
+  }
+  
+  commands.push({
+    command: CommandType.IF,
+    argument: condition
+  });
+
+  return {
+    then: _then,
+  };
+
+}
