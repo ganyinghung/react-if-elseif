c1383220479439041ae3ca181df0f0b75f4e80de
Gan <github@yhg.io>
2024-05-04 19:20:24 +0100

    New build

--- dist/index.js
+++ dist/index.js
@@ -52,6 +52,12 @@ function _if(condition) {
                     if (last_condition === true) {
                         return commands[i].argument;
                     }
+                    if (i === commands.length - 1) {
+                        // there is no ELSE in the statements
+                        // but the IF condition is not satisified
+                        // so we simply return null
+                        return null;
+                    }
                     break;
                 case CommandType.ELSE:
                     if (last_condition === false) {
